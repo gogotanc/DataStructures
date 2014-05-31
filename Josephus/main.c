9125c59b7a29ae25a1d0dc9bf4e40eba39ce3a4a
gogotanc <gogotanc@163.com>
2014-05-31 19:14:23 +0800

    约瑟夫问题
    完成了约瑟夫问题的编写

--- Josephus/main.c
+++ Josephus/main.c
@@ -44,7 +44,47 @@ status initList( LinkList *L )
         p = q;
     }
 
+    q->next = (*L)->next;
+
     return OK;
+}
+
+//依次报数，报3的人自杀。
+status order4Kill( LinkList *L )
+{
+    int i = 0;
+    int j = 1;
+
+    Node *p, *q;
+
+    p = (*L);
+
+    while( TRUE )
+    {
+        i++;
+        p = p->next;
+
+        if( !p )
+            return OK;
+
+        if( i==2 )
+        {
+            q = p->next;
+
+            p->next = p->next->next;
+            p = p->next;
+
+            printf("%d,", q->data);
+            q->next = NULL;
+            free(q);
+
+            if( j%13 == 0 )
+                printf("\n");
+
+            i = 1;
+            j++;
+        }
+    }
 }
 
 int main()
@@ -52,6 +92,8 @@ int main()
     LinkList list = NULL;
 
     initList(&list);
+
+    order4Kill(&list);
 
     return 0;
 }
