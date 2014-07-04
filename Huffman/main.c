6df7548099400d1c2daf3cb9165cf484ef3770e5
gogotanc <gogotanc@163.com>
2014-07-04 11:28:13 +0800

    实现了Huffman编码过程

--- Huffman/main.c
+++ Huffman/main.c
@@ -11,65 +11,238 @@ typedef struct Node
 {
     ElemType data;
     int num;
+    char *code;
     struct Node *l_chlid;
     struct Node *r_child;
     struct Node *next;
 }Node, *HufQueue, *HufTree;
 
+void printCode( HufTree *T )
+{
+    Node *n;
+    n = (*T);
+
+    if( n->l_chlid == NULL && n->r_child == NULL )
+    {
+        printf("元素 %c 编码为 \" %s \". \n", n->data, n->code );
+    }
+    else
+    {
+        printCode( &n->l_chlid );
+        printCode( &n->r_child );
+    }
+}
+
+//给字符串添加字符
+char * addCode( char *str, char code )
+{
+    int i;
+    char *buf;
+    int len = strlen( str );
+    buf = (char *)malloc( len * sizeof(char));
+
+    for( i=0; i<len; i++ )
+    {
+        buf[i] = str[i];
+    }
+    buf[len] = code;
+    buf[len+1] = '\0';
+    return buf;
+}
+
+//对Huffman树进行编码
+void encode( Node **n, char *c )
+{
+    if( c == NULL )
+    {
+        c = (char *)malloc(sizeof(char));
+        c[0] = '\0';
+    }
+
+    Node *x;
+    x = (*n);
+    char l_code = '0';
+    char r_code = '1';
+
+    x->code = (char *)malloc(sizeof(char));
+    x->code = c;
+
+    if( x->l_chlid != NULL )
+    {
+        encode( &x->l_chlid, addCode( c, l_code ) );
+    }
+    if( x->r_child != NULL )
+    {
+        encode( &x->r_child, addCode( c, r_code ) );
+    }
+}
+
 //将元素放入合适的位置，按大小顺序排列
 void push( HufQueue *Q, Node **n )
 {
-    Node *x, *y;
+    HufQueue x = NULL;
+    Node *y;
     x = (*Q);
+
+    if( x->next == NULL )
+    {
+        x->next = (*n);
+        return;
+    }
+
     while( x->next != NULL )
     {
         y = x;
         x = x->next;
 
-        if( (*n)->num < x->num )
+        if( (*n)->num <= x->num )
         {
             (*n)->next = x;
             y->next = (*n);
+            break;
+        }
+
+        if( x->next == NULL )
+        {
+            x->next = (*n);
             break;
         }
     }
 }
 
 //获取最小的元素，也就是第一个元素。
-void get( HufQueue Q, Node **n )
+void get( HufQueue *Q, Node **n )
 {
-    (*n) = Q->next;
-}
+    HufQueue q = NULL;
+    q = (*Q);
 
-//带权的链表生成Huffman树
-void createTree( HufTree *T, HufQueue *Q )
-{
-    Node *x, *y;
-    Node *n;
-
-    (*T) = (HufTree)malloc(sizeof(Node));
-
-    get( *Q, &x );
-    get( *Q, &y );
-
-    n->num = x->num + y->num;
-
-    if( x->num < y->num )
+    if( q->next != NULL )
     {
-        n->l_chlid = x;
-        n->r_child = y;
+        (*n) = q->next;
+        q->next = q->next->next;
     }
     else
     {
-        n->l_chlid = y;
-        n->r_child = x;
+        (*n) = NULL;
     }
-    n->data = '\\';
-    n->next = NULL;
+}
 
-    push( Q, &n );
+//带权的链表生成Huffman树  想递归实现
+void createTree( HufTree *T, HufQueue *Q )
+{
+    HufQueue q = NULL;
+    HufTree t = NULL;
+    Node *x, *y;
+    Node *n;
 
+    q = (*Q);
+    t = (*T);
 
+    get( &q, &x );
+    get( &q, &y );
+
+    //test
+    printf("\n--------------------------------\n");
+    if( x != NULL)
+    {
+        printf("x data: %c = %d ", x->data,x->num);
+    }
+    printf(" ... ");
+    if( y != NULL )
+    {
+        printf("y data: %c = %d ", y->data,y->num);
+    }
+    printf("\n--------------------------------\n");
+
+    if( x != NULL && y == NULL )
+    {
+        (*T) = (HufTree)malloc(sizeof(Node));
+        (*T)->next = x;
+    }
+    else
+    {
+        n = (Node *)malloc(sizeof(Node));
+
+        n->num = x->num + y->num;
+
+        if( x->num < y->num )
+        {
+            n->l_chlid = x;
+            n->r_child = y;
+        }
+        else
+        {
+            n->l_chlid = y;
+            n->r_child = x;
+        }
+        n->data = '\\';
+        n->next = NULL;
+
+        push( &q, &n );
+
+        createTree( &t, &q );
+    }
+}
+
+//带权的链表生成Huffman树  迭代实现
+void createTree2( HufTree *T, HufQueue *Q )
+{
+    HufQueue q = NULL;
+    Node *x, *y;
+    Node *n;
+
+    q = (*Q);
+
+    get( &q, &x );
+    get( &q, &y );
+
+    while( 1 )
+    {
+        //test
+        /*
+        printf("\n--------------------------------\n");
+        if( x != NULL)
+        {
+            printf("x data: %c = %d ", x->data,x->num);
+        }
+        printf(" ... ");
+        if( y != NULL )
+        {
+            printf("y data: %c = %d ", y->data,y->num);
+        }
+        printf("\n--------------------------------\n");
+        */
+
+        if( x != NULL && y == NULL )
+        {
+            (*T) = (HufTree)malloc(sizeof(Node));
+            (*T) = x;
+            break;
+        }
+
+        n = (Node *)malloc(sizeof(Node));
+
+        n->num = x->num + y->num;
+        n->data = '\\';
+        n->code = NULL;
+        n->next = NULL;
+
+        if( x->num < y->num )
+        {
+            n->l_chlid = x;
+            n->r_child = y;
+        }
+        else
+        {
+            n->l_chlid = y;
+            n->r_child = x;
+        }
+
+        push( &q, &n );
+
+        get( &q, &x );
+        get( &q, &y );
+    }
 }
 
 //初始化原始队列
@@ -89,6 +262,7 @@ void initQueue( HufQueue *q, char (*cou)[2] )
     (*q)->l_chlid = (*q)->r_child =(*q)->next = NULL;
     (*q)->num = len;
     (*q)->data = '#';
+    (*q)->code = NULL;
 
     x = (*q);
 
@@ -97,6 +271,7 @@ void initQueue( HufQueue *q, char (*cou)[2] )
         y = (Node *)malloc(sizeof(Node));
         y->data = cou[j][0];
         y->num = cou[j][1];
+        y->code = NULL;
         y->l_chlid = y->r_child = y->next =NULL;
         x->next = y;
         x = y;
@@ -190,11 +365,15 @@ void mySort( char (*cou)[2] )
 
 int main()
 {
-    int i, len;
+    //int i, len;
     char str[256];
     char cou[128][2];
+    char *flag = NULL;
+
     HufQueue Q = NULL;
     HufTree T = NULL;
+
+    printf("请输入您需要编码的讯息：\n");
 
     getInput( str );
 
@@ -202,7 +381,52 @@ int main()
 
     mySort( cou );
 
-    //打印一下排好序的原始数组。
+    //初始化带权的链表。
+    initQueue( &Q, cou );
+
+    //带权的链表生成Huffman树
+    createTree2( &T, &Q );
+
+    //对各个字符进行编码
+    encode( &T, flag );
+
+    //打印每个元素的编码
+    printCode( &T );
+
+    return 0;
+}
+
+
+    /*
+    测试push函数
+    Node *n1 = NULL;
+    n1 = (Node *)malloc(sizeof(Node));
+    n1->l_chlid = n1->r_child = n1->next = NULL;
+    n1->data = '\\';
+    n1->num = 2;
+    push( &Q, &n1 );
+
+    while( Q->next != NULL )
+    {
+        Q = Q->next;
+        printf("data: %c = %d \n", Q->data,Q->num);
+    }
+    */
+
+    /*
+    测试get函数
+    Node *n2 = NULL;
+    n2 = (Node *)malloc(sizeof(Node));
+    get(&Q,&n2);
+    while( Q->next != NULL )
+    {
+        Q = Q->next;
+        printf("data: %c = %d \n", Q->data,Q->num);
+    }
+    */
+
+        /*
+    打印一下排好序的原始数组。
     len = cou[0][1];
     for( i=0; i<len+1; i++ )
         printf("%c ", cou[i][0]);
@@ -210,38 +434,4 @@ int main()
     for( i=0; i<len+1; i++ )
         printf("%d ", cou[i][1]);
     printf("\n");
-
-    //初始化带权的链表。
-    initQueue( &Q, cou );
-
-
-
-    /* 测试push函数
-    Node *n = NULL;
-    n = (Node *)malloc(sizeof(Node));
-    n->l_chlid = n->r_child = n->next = NULL;
-    n->data = '\\';
-    n->num = 2;
-    push( &Q, &n );
-
-    while( Q->next != NULL )
-    {
-        Q = Q->next;
-        printf("%d", Q->num);
-    }
     */
-
-    /* 测试get函数
-    printf("%d", Q->next->next->num);
-
-    Node *n = NULL;
-    n = (Node *)malloc(sizeof(Node));
-    get(Q,&n);
-    printf("%d",n->num);
-    */
-
-    //带权的链表生成Huffman树
-    createTree( &T, &Q );
-
-    return 0;
-}
